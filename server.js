c8acf71789808ba6a6e17ee37febea324fc4f358
sdreyes <sheshechan90@gmail.com>
2019-02-17 10:53:59 -0500

    Adjust route directory calls

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
