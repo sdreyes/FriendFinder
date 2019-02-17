757b95c5c1792aee7ed34f5c6e891559924cf8eb
sdreyes <sheshechan90@gmail.com>
2019-02-17 11:41:48 -0500

    Add default route

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -5,11 +5,11 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
 
-    app.get("/home" || "*", function(req, res) {
+    app.get("/home", function(req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
     
-    // app.get("*", function(req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/home.html"));
-    // });
+    app.get("*", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/home.html"));
+    });
 }
