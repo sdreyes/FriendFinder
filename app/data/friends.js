b9df64c00703e549079d7218670d400203ccdff4
sdreyes <sheshechan90@gmail.com>
2019-02-19 18:37:33 -0500

    Adjust scores

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,12 +2,12 @@ var friends = [
     {
         name: "Shelby",
         photo: "https://sdreyes.github.io/assets/images/shelbyreyes.jpg",
-        scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+        scores: [1, 3, 2, 5, 3, 1, 4, 2, 2, 5]
     },
     {
         name: "Sam",
         photo: "https://scontent.fphl2-1.fna.fbcdn.net/v/t1.0-9/46112787_10102030478860929_6427518382093893632_n.jpg?_nc_cat=107&_nc_ht=scontent.fphl2-1.fna&oh=c5fbacb659081ac534d11a9d014162c5&oe=5D27A7C0",
-        scores: [5, 5, 5, 5, 5, 5, 5, 5, 5, 5]
+        scores: [3, 5, 1, 1, 1, 2, 4, 4, 3, 2]
     }
 ];
 
